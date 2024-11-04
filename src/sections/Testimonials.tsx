db4e082253eb60e59b7f5ccb16de7f6dd2f7f5f5
Joeeazy <joeeazy34@gmail.com>
2024-11-04 23:47:00 +0300

    Testimonials section created

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -66,5 +66,16 @@ const testimonials = [
 ];
 
 export const Testimonials = () => {
-  return null;
+  return (
+    <section>
+      <div className="container">
+        <div>Testimonials</div>
+        <h2>What our users say</h2>
+        <p>
+          From intuitive design to powerful features, our app has become an
+          essential tool for users around the world
+        </p>
+      </div>
+    </section>
+  );
 };
